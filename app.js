dc6b39d0b7c20256c23e0d19f46d8b4bd83660d6
pablwoAraujo <pablwo.araujo@ccc.ufcg.edu.br>
2019-11-04 15:18:54 -0300

    case1

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-function cadastraUsuario() {
-    let nome = document.querySelector("#newNome");
-    let nota = document.querySelector("#newNota");
-    fetch('https://lab01-projsw-ufcg.herokuapp.com/api/disciplinas', {
-        'method': 'POST',
-        'body': `{"nome": "${nome.value}", "nota": ${nota.value}}`,
-        'headers': {'Content-Type': 'application/json'}
-    })
-    .then(r => r.json())
-    .then(d => {
-        disciplinas.push(d);
-        fetch_disciplinas(disciplinas);
-    });
-}
+let $createButton = document.querySelector("#create");
+$createButton.addEventListener('click', 
+
+    function cadastraUsuario() {
+        let primeiroNome = document.querySelector("#newPrimeiroNome");
+        let email = document.querySelector("#newEmail");
+        let ultimoNome = document.querySelector("#newUltimoNome");
+        let cartaoCredito = document.querySelector("#newCartaoCredito");
+        let senha = document.querySelector("#newSenha");
+        fetch("http://localhost:8080/api/usuarios", {
+            'method': 'POST',
+            'body': `{"email": "${email.value}","primeiroNome": "${primeiroNome.value}","ultimoNome": "${ultimoNome.value}",
+            "cartaoCredito": "${cartaoCredito.value}","senha": "${senha.value}" }`,
+            'headers': {'Content-Type': 'application/json'}
+        })
+        .then(r => r.json())
+        .then(r => {console.log(r)});
+    }
+);
