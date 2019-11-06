3e8b0b21ba3014d1efe4496bbef90f75c543b1b7
pablwoAraujo <pablwo.araujo@ccc.ufcg.edu.br>
2019-11-06 14:02:42 -0300

    caso2

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ let $main = document.querySelector('#main');
       view1();
     } else if (["#view2"].includes(hash)) {
       view2();
-    }
+    } 
   }());
 
 async function fetch_status() {
@@ -17,13 +17,14 @@ async function fetch_status() {
   return data;
 }
 
-let template1, template2;
+let template1, template2, template3;
 async function fetch_templates() {
   let html_templates = await (fetch('templates.html').then(r => r.text()));
   let e = document.createElement("div");
   e.innerHTML = html_templates;
   template1 = e.querySelector('#cadastro');
   template2 = e.querySelector('#login');
+  template3 = e.querySelector('#cadastroCampanha');
 }
 
 let $menuLogin = document.querySelector('#viewLogin');
@@ -31,6 +32,9 @@ $menuLogin.addEventListener('click', view2);
 
 let $menuCadastro = document.querySelector('#viewCadastro');
 $menuCadastro.addEventListener('click', view1);
+
+let $menuCadastroCampanha = document.querySelector('#viewCadastroCampanha');
+$menuCadastroCampanha.addEventListener('click', view3);
 
 function view1(){
     let $template = template1;
@@ -58,6 +62,8 @@ function view1(){
     $a.addEventListener('click', view2);
 }
 
+let token = null;
+
 function view2() {
     let $template = template2;
     $main.innerHTML = $template.innerHTML;
@@ -73,9 +79,34 @@ function view2() {
                     'headers': {'Content-Type': 'application/json'}
                 })
                 .then(r => r.json())
-                .then(r => {console.log(r)});
+                .then(r => {token = r.token});
+                //.then(r => {window.console.log((r).token)});
             }
         );
     let $a = document.querySelector('#link');
     $a.addEventListener('click', view1);
+}
+
+function view3(){
+    let $template = template3;
+    $main.innerHTML = $template.innerHTML;
+
+    let $cadastraButton = document.querySelector("#cadastraCampanha");
+    $cadastraButton.addEventListener('click', 
+    function cadastraCampanha(){
+        let nomeCampanha = document.querySelector("#newNomeCampanha");
+        let descricaoCampanha = document.querySelector("#newDescricaoCampanha");
+        let metaCampanha = document.querySelector("#newMetaCampanha");
+        fetch("http://localhost:8080/api/campanhas", {
+                    'method': 'POST',
+                    'credentials': 'include',
+                    'body': `{"nome": "${nomeCampanha.value}","descricao": "${descricaoCampanha.value}", 
+                    "meta": "${metaCampanha.value}"}`,
+                    'headers': {'Content-Type': 'application/json', 'Authorization': `Bearer ${token.value}`}
+                })
+                .then(r => r.json())
+                .then(r => {console.log(r)});
+        }
+    );
+
 }
